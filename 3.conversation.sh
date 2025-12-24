a2ac701250f0383a3fc43a23161a4c384f2969c9
BHARAT <bharataws22@gmail.com>
2025-12-24 11:08:40 +0530

    Hellow world started

---
+++ 3.conversation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo " sandeep: Hi Bharat How are you"
+echo " Bharat: Yeah I am fine. How About you."
+echo " sandeep: Thanks I am good. where are you going"
+echo " Bharat: I am going to Kia Show room"
+echo " sandeep: Is it! which car you are going to buy?"
+echo " Bharat: I am going to Buy Kia Seltos GT Line"
+echo " sandeep: oh thats great. Have a nice car and enjoy the ride"
+echo " Bharat: oh thankyou very much"
