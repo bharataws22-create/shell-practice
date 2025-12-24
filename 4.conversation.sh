b41f2213455f4d249a693766257d38ead410993b
BHARAT <bharataws22@gmail.com>
2025-12-24 11:16:47 +0530

    Hellow world started

---
+++ 4.conversation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+person1=sandeep
+person2=Bharat
+
+echo " $person1: Hi Bharat How are you"
+echo " $person2: Yeah I am fine. How About you."
+echo " $person1: Thanks I am good. where are you going"
+echo " $person2: I am going to Kia Show room"
+echo " $person1: Is it! which car you are going to buy?"
+echo " $person2: I am going to Buy Kia Seltos GT Line"
+echo " $person1: oh thats great. Have a nice car and enjoy the ride"
+echo " $person2: oh thankyou very much"
